227ea80c2491171a8d48eb52a8b22b295ffad344
Da Costa Music <dev@dacostamusic.com>
2025-10-16 20:51:26 +0000

    Remover MarqueeText da home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,11 @@ import AboutSection from "@/components/about-section"
 import RosterSection from "@/components/roster-section"
 import MusicSection from "@/components/music-section"
 import ContactSection from "@/components/contact-section"
-import MarqueeText from "@/components/marquee-text"
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSection />
-
-      <MarqueeText />
 
       <AboutSection />
 
