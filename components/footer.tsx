eb1b20d020bf9533732046a220f8b390160c736d
Da Costa Music <dev@dacostamusic.com>
2025-10-28 15:48:43 +0000

    Remove link de eventos do footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -115,11 +115,6 @@ export default function Footer() {
                 </Link>
               </li>
               <li>
-                <Link href="/events" className="text-white/70 hover:text-white">
-                  Events
-                </Link>
-              </li>
-              <li>
                 <Link href="/#contact" className="text-white/70 hover:text-white">
                   Contact
                 </Link>
